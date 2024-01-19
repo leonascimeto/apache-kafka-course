8a4d63efa96bf1919e32f01a11a424411d5d41fb
leo <nascimentoleo899@gmail.com>
2024-01-19 19:43:54 -0300

    implement custom configuration for consumer kafka

--- str-consumer/src/main/java/tech/leondev/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/tech/leondev/strconsumer/listeners/StrConsumerListener.java
@@ -2,14 +2,26 @@ package tech.leondev.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+import tech.leondev.strconsumer.custom.StrConsumerCustomListener;
 
 @Component
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(topics = "str-topic", groupId = "group-1", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Message received: {}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Message received: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Message received: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Message received: {}", message);
     }
 }
